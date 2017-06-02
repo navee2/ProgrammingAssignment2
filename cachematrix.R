5ed32c1fdfc518e90b99123ea4e0ea29fd0e0358
navee2 <naveeninkgp@gmail.com>
2017-06-02 09:07:21 +0530

    changes according to programming task

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Following code retuns inverse of an invertible matrix
+## If inverse has already been calculated it doesn't calculate it again
+"Test :-
+z<- matrix(rnorm(4),2,2)
+m<- makeCacheMatrix(z)
+cacheSolve(m) ## matrix inverse calculation takes here
+cacheSolve(m) ## This time cache output is printed
+solve(z) ## to check correctness of output"
+  
 
-## Write a short comment describing this function
+### Function makeCacheMatrix assigns getters and setters
+### There is a getter and setter for matrix and another pair for inverse
+### the list of functions returned here is then used in cachesolve
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  #setter for matrix
+  set <- function(y) {
+    x <<- y ### assign value in parent environment
+    m <<- NULL
+  }
+  #getter for matrix
+  get <- function() x
+  #setter for inverse
+  setinv <- function(inv) m <<- inv
+  #getter for inverse
+  getinv <- function() m
+  #retuns the list containing four functions
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Following function check if inverse has already been calculated.
+## If so it retuns the calculated inverse else returns newly calculated
+## return
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ##Checking if there exists inverse in the environment
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m) ## returning preexisting inverse
+  }
+  ## calculate inverse as no pre-existing inverse found
+  data <- x$get()
+  m <- solve(data)
+  x$setinv(m)
+  m
 }
